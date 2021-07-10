8b9ecffd0879070415e55a40082d3db3530e0ce7
Sergei Rachmiroff <rachmiroff_s@auca.kg>
2021-07-10 09:15:41 +0200

    Add messaging logic (Closes #6)
    - [x] Add message creation handlers for the POST request
    - [x] Add error checking logic for message creation
    - [x] Improve the index page route to query and list the messages

--- mems.js
+++ mems.js
@@ -2,8 +2,9 @@ import express from 'express'
 import session from 'express-session'
 import dotenv from 'dotenv'
 
-import { User, dbStart } from './src/db.js'
+import { User, Message, dbStart } from './src/db.js'
 import users from './src/users.js'
+import messages from './src/messages.js'
 
 dotenv.config()
 
@@ -12,20 +13,21 @@ app.use(express.static('public'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(session({
-  secret: process.env.MEMS_SESSION_SECRET,
-  resave: false,
-  saveUninitialized: true
+    secret: process.env.MEMS_SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true
 }))
 app.handleError = (res, status, error = 'Something went wrong') => {
-  return res.status(status).json({ error })
+    return res.status(status).json({ error })
 }
 
 const host = process.env.MEMS_HOST || 'localhost'
 const port = process.env.MEMS_PORT || 8080
 
 users(app, User)
+messages(app, Message, User)
 
 dbStart()
 app.listen(port, () => {
-  console.log(`mems server is listening at http://${host}:${port}`)
+    console.log(`mems server is listening at http://${host}:${port}`)
 })
--- src/db.js
+++ src/db.js
@@ -9,9 +9,9 @@ const db = new Sequelize(
     process.env.DB_USER,
     process.env.DB_PASS,
     {
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
-      dialect: process.env.DB_DIALECT
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: process.env.DB_DIALECT
     }
 )
 
